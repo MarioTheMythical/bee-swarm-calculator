413e1bce22d87a09aa6806b5a500039f16451566
ConorG1247 <conorgoddard1247@gmail.com>
2022-09-30 16:23:31 +0100

    created theme context

--- src/components/Interface/Main.tsx
+++ src/components/Interface/Main.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState, createContext } from "react";
 import { inventoryItems, inventoryMaterials } from "libs/data";
-import { useEffect, useState } from "react";
 import ItemDisplay from "./ItemDisplay";
 import ItemSelection from "./ItemSelection";
+
+export const ThemeContext = createContext(true);
 
 function Main() {
   const [displaySelection, setDisplaySelection] = useState<number>(0);
@@ -41,38 +43,40 @@ function Main() {
   }, []);
 
   return (
-    <div className="main-parent-container">
-      <div className="main-container">
-        <div className="main-title-container">
-          <div className="main-title" />
-          <div
-            onClick={() => changeTheme(!theme)}
-            className={theme ? "dark" : "light"}
-          />
-        </div>
-        <div className="main-subtitle">
-          A recipe calculator which makes it easier to track your progress.
-        </div>
-        <div className="main-items-container">
-          <ItemSelection
-            displaySelection={displaySelection}
-            changeUserDisplaySelection={changeUserDisplaySelection}
-          />
-          {bssDescriptionCheck && (
-            <div className="main-description">
-              To get started using BSS Calculator, enter your current item and
-              material amounts within the{" "}
-              <span style={{ fontWeight: "bold", color: "green" }}>
-                Inventory
-              </span>{" "}
-              tab <br /> and then select which recipe you'd like to check using
-              the buttons above.
-            </div>
-          )}
-          <ItemDisplay displaySelection={displaySelection} />
+    <ThemeContext.Provider value={theme}>
+      <div className="main-parent-container">
+        <div className="main-container">
+          <div className="main-title-container">
+            <div className="main-title" />
+            <div
+              onClick={() => changeTheme(!theme)}
+              className={theme ? "dark" : "light"}
+            />
+          </div>
+          <div className="main-subtitle">
+            A recipe calculator which makes it easier to track your progress.
+          </div>
+          <div className="main-items-container">
+            <ItemSelection
+              displaySelection={displaySelection}
+              changeUserDisplaySelection={changeUserDisplaySelection}
+            />
+            {bssDescriptionCheck && (
+              <div className="main-description">
+                To get started using BSS Calculator, enter your current item and
+                material amounts within the{" "}
+                <span style={{ fontWeight: "bold", color: "rgb(53, 151, 97)" }}>
+                  Inventory
+                </span>{" "}
+                tab <br /> and then select which recipe you'd like to check
+                using the buttons above.
+              </div>
+            )}
+            <ItemDisplay displaySelection={displaySelection} />
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Items/ItemSelection.tsx
+++ src/components/Items/ItemSelection.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { itemTypes } from "libs/types";
+import { ThemeContext } from "components/Interface/Main";
 
 type Props = {
   itemData: itemTypes;
@@ -11,6 +13,8 @@ function ItemSelection({
   itemSelection,
   changeItemSelection,
 }: Props) {
+  const theme = useContext(ThemeContext);
+
   return (
     <div className="items-titles-container">
       {itemData.map((item, index) => {
