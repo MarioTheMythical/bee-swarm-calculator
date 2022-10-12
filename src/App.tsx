9e32ee21a87537d4dccce325479c651c978243c3
ConorG1247 <conorgoddard1247@gmail.com>
2022-10-12 17:06:19 +0100

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
-import Main from "components/Interface/Main";
 
 function App() {
   const [theme, setTheme] = useState(
